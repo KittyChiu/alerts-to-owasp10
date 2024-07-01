0c683c08b75bf2b685777b3bba7092f353260389
Kitty Chiu <42864823+KittyChiu@users.noreply.github.com>
2024-07-01 21:05:13 +1000

    chore: upgraded dependencies

--- dist/index.js
+++ dist/index.js
@@ -3027,7 +3027,7 @@ __export(dist_src_exports, {
 module.exports = __toCommonJS(dist_src_exports);
 
 // pkg/dist-src/version.js
-var VERSION = "4.0.0";
+var VERSION = "4.0.1";
 
 // pkg/dist-src/index.js
 function requestLog(octokit) {
